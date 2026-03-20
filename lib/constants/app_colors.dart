5c2461165e681c2b733448f70df8fae04b4be85a
batuhanars <batuhan.ars@yahoo.com>
2026-03-20 21:38:21 +0300

    Font ve renk şeması ayarlandı.

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary
+  static const Color primary = Color(0xFF176FF2);
+
+  // Text Colors
+  static const Color textPrimary = Color(0xFF232323);
+  static const Color textSecondary = Color(0xFF767676);
+  static const Color textLight = Color(0xFFB8B8B8);
+  static const Color textWhite = Color(0xFFFFFFFF);
+
+  // Background and Fill Colors
+  static const Color backgroundWhite = Color(0xFFFFFFFF);
+  static const Color fillBlue = Color(0xFFF3F8FE); // Arama çubuğu, pasif buton ve ikon arkaplanları
+
+  // System & Accent Colors
+  static const Color heartRed = Color(0xFFEC5655);
+  static const Color priceGreen = Color(0xFF2DD7A4);
+  static const Color ratingStar = Color(0xFFF8D675);
+}
